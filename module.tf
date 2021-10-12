10bb327b66eed01df302becc05786d4ed218471a
DaShaun <dashaun@redislabs.com>
2021-10-11 23:26:12 -0500

    add modules

---
+++ module.tf
@@ -0,0 +1,7 @@
+module "acre-terraform-simple" {
+  source = "github.com/redis-developer/acre-terraform-simple"
+}
+
+module "acre-terraform" {
+  source = "github.com/redis-developer/acre-terraform"
+}
