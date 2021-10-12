722b7e7126e65fc9f8c82c4e690050ae0d245d06
DaShaun <dashaun@redislabs.com>
2021-10-11 23:02:26 -0500

    update acr name, add .gitignore

--- azurerm_container_registry.tf
+++ azurerm_container_registry.tf
@@ -1,5 +1,5 @@
 resource "azurerm_container_registry" "acr" {
-  name                = "containerRegistry1"
+  name                = format("redisgeek%s", random_string.resource_group_name.result)
   resource_group_name = azurerm_resource_group.resource_group.name
   location            = azurerm_resource_group.resource_group.location
   sku                 = "Premium"
