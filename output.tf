41e95c5ed1f15b04d6b71e8011c3847aa24e0b8d
DaShaun <dashaun@redislabs.com>
2021-10-14 17:52:03 -0500

    add output

--- output.tf
+++ output.tf
@@ -10,3 +10,8 @@ output "redisgeek_config" {
   value     = module.cloud-pipeline-helm.redisgeek_config
   sensitive = true
 }
+
+output "a_aks_get_credentials" {
+  value       = "az aks get-credentials --resource-group  ${module.cloud-pipeline-helm.resource_group} --name ${module.cloud-pipeline-helm.cluster_name}"
+  description = "command to get kube config via az aks cli"
+}
